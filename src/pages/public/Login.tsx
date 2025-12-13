c6aa92543009658c11600e09784130b89ca3b559
niladripradhan <niladrip347@gmail.com>
2025-12-13 20:08:35 +0530

    update

--- src/pages/public/Login.tsx
+++ src/pages/public/Login.tsx
@@ -51,7 +51,7 @@ export default function Login() {
       dispatch(setLoading(true))
 
       const res = await axios.post(
-        `${USER_API_ENDPOINT}/login`,
+        `http://localhost:5000/api/v1/user/login`,
         {
           email: formData.email,
           password: formData.password,
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
-export const USER_API_ENDPOINT = import.meta.env.VITE.VITE_USER_API_ENDPOINT
-export const JOB_API_ENDPOINT = import.meta.env.VITE.VITE_JOB_API_ENDPOINT
-export const APPLICATION_API_ENDPOINT = import.meta.env.VITE.VITE_APPLICATION_API_ENDPOINT
-export const COMPANY_API_ENDPOINT = import.meta.env.VITE.VITE_COMPANY_API_ENDPOINT
-export const REGISTER_COMPANY_API_ENDPOINT = import.meta.env.VITE.VITE_REGISTER_COMPANY_API_ENDPOINT
+export const USER_API_ENDPOINT = import.meta.env.VITE_USER_API_ENDPOINT || "http://localhost:5000/api/v1/user/login"
+export const JOB_API_ENDPOINT = import.meta.env.VITE_JOB_API_ENDPOINT
+export const APPLICATION_API_ENDPOINT = import.meta.env.VITE_APPLICATION_API_ENDPOINT
+export const COMPANY_API_ENDPOINT = import.meta.env.VITE_COMPANY_API_ENDPOINT
+export const REGISTER_COMPANY_API_ENDPOINT = import.meta.env.VITE_REGISTER_COMPANY_API_ENDPOINT
